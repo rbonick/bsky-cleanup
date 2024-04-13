466e11d874f443fa8e10cc95bca6e03fa1058e68
Ryan Bonick <rjbman93@gmail.com>
2024-04-13 17:40:32 -0600

    lmfao committed password... at least bsky lets me revoke it so i don't need to redo the whole repo!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,15 @@
 import { AppBskyFeedDefs, AppBskyFeedPost, AtpAgentLoginOpts } from "@atproto/api";
 
 import { agent } from "~/lib/api";
+import { loginCredentials } from "~/secrets/login";
 
 const EXAMPLE_POST =
   "at://did:plc:vwzwgnygau7ed7b7wt5ux7y2/app.bsky.feed.post/3karfx5vrvv23";
 
 export default async function Homepage() {
+  await agent.login(loginCredentials)
+  
   const accountLogin = 'ryanbonick.com'
-
-  await agent.login({
-    identifier: accountLogin,
-    password: 'oyr7-luoe-77q5-quid'
-  })
 
   const { data: profileData } = await agent.app.bsky.actor.getProfile({ actor: accountLogin })
   const { data: { feed } } = await agent.app.bsky.feed.getAuthorFeed({ actor: accountLogin })
